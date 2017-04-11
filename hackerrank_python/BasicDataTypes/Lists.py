79aa46199b463c0b7fe28d0d309cd7af1775e753
Eva Huang <eva.huang@capitalone.com>
2017-04-10 23:45:22 -0500

    few questions added to BasicDataTypes

---
+++ hackerrank_python/BasicDataTypes/Lists.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    n = int(input())
+    l = []
+    
+    for _ in range(n):
+        s = input().split()
+        cmd = s[0]
+        args = s[1:]
+        
+        if cmd !="print":
+            cmd += "("+ ",".join(args) +")"
+            #print (cmd)
+            eval("l."+cmd)
+        else:
+            print (l)
